c0e0667b80b71d3d91cc234f605ea29dd68ac66c
michaelLevin5908 <michaeljustinlevin@gmail.com>
2026-05-16 16:02:21 -0700

    chore(cask): bump dripos to v0.1.16

--- Casks/drip.rb
+++ Casks/drip.rb
@@ -1,14 +1,14 @@
 cask "drip" do
-  version "0.1.11"
-  sha256 "6e0e19448f967106c0d31acb6dc3ea87f8205a2dbfc388ac04af389c882f2f27"
+  version "0.1.16"
+  sha256 "66a436a3ab6fdf6327d5a018af52cc05f67972fb6d8a7acce0d069a17a4c8d76"
 
-  url "https://github.com/DripYCx26/drip-releases/releases/download/v#{version}/memra_#{version}_universal.dmg",
+  url "https://github.com/DripYCx26/drip-releases/releases/download/v#{version}/dripos_#{version}_universal.dmg",
       verified: "github.com/DripYCx26/"
-  name "memra"
+  name "dripos"
   desc "Local-first customer intelligence: Stripe + Slack + Gmail by customer"
   homepage "https://drippay.dev/"
 
-  app "memra.app"
+  app "dripos.app"
 
   zap trash: [
     "~/Library/Application Support/dev.drippay.app",
