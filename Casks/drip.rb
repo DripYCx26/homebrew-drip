e993b95954ff11d55bed1ee96e4a44aef874bdfe
michaelLevin5908 <michaeljustinlevin@gmail.com>
2026-05-16 15:47:59 -0700

    chore(cask): bump dripos to v0.1.15

--- Casks/drip.rb
+++ Casks/drip.rb
@@ -1,6 +1,6 @@
 cask "drip" do
-  version "0.1.14"
-  sha256 "12bea6826528c8f139411bc3c6afe0893d3b8e66b717c260dcd278618b8f0536"
+  version "0.1.15"
+  sha256 "b0ffd6db75cc05e6b5a33dd0572117f17212c5db4b22f068568021a8a4d4be7c"
 
   url "https://github.com/DripYCx26/drip-releases/releases/download/v#{version}/dripos_#{version}_universal.dmg",
       verified: "github.com/DripYCx26/"
