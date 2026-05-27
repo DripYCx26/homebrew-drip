a1cc8f8869094458853ef7e6770d70950b1bbf8c
Michael Levin <135309004+MichaelLevin5908@users.noreply.github.com>
2026-05-26 23:34:17 -0700

    Update cask to v1.2.9

--- Casks/drip.rb
+++ Casks/drip.rb
@@ -1,6 +1,6 @@
 cask "drip" do
-  version "1.2.8"
-  sha256 "2cd2d6c8b77d8cef9e517703c0218eca8045061c6e51790befa24c7e23eab273"
+  version "1.2.9"
+  sha256 "496986c7b6b30b4a0d13856eaaad6a53c508272c33cfabf4bf93469acde8b370"
 
   url "https://github.com/DripYCx26/drip-releases/releases/download/v#{version}/dripos_#{version}_universal.dmg",
       verified: "github.com/DripYCx26/"
